946664ce4fa8ff6f7676b968468f356011a3e3ab
Computerino11 <82869643+Computerino11@users.noreply.github.com>
2022-05-14 18:19:33 +0200

    Create style.cc

---
+++ style.cc
@@ -0,0 +1,42 @@
+html, body {
+  height: 100%;
+  width: 100%;
+  margin: 0px;
+  padding: 0px;
+}
+
+
+#schermo {
+  background-color: DodgerBlue;
+  height: 100%;
+  width: 100%;
+}
+
+#titolo {
+  font-size: 10vw;
+  text-align: center;
+  padding-top: 15vw;
+}
+
+#info {
+    text-align: center;
+  margin-top: 15vw;
+}
+
+#myText {
+             display: block;
+             margin : 0 auto;
+  margin-top: 15vw;
+}
+
+#btn {
+             display: block;
+             margin : 0 auto;
+  width: 40%;
+  margin-top: 15vw;
+}
+
+#box2 {
+    text-align: center;
+  margin-top: 15vw;
+}
